fd2720097c3609dee21750071c2f8bccea9f6fbc
Carlos Varalla <cvaralla@gmail.com>
2026-01-19 07:48:56 -0300

    chore(web/prototype): remove leftover figma_extracted tree

--- web/prototype/figma_extracted/src/modules/transform/core/index.ts
+++
@@ -1,9 +0,0 @@
-/**
- * API pública del core Transform
- * 
- * Este módulo es headless (sin React, sin DOM, sin CSS)
- * y puede usarse en cualquier entorno gráfico.
- */
-
-export * from './types';
-export * from './engine';
